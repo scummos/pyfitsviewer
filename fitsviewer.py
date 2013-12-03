46e63673b2fe305da2dda18c194a68c2ee16010e
Sven Brauch <svenbrauch@googlemail.com>
2013-12-03 22:21:26 +0100

    basic functionality is working.

--- fitsviewer.py
+++ fitsviewer.py
@@ -1,6 +1,6 @@
 from PyQt4.QtGui import QMainWindow
 from PyQt4.QtGui import QApplication
-from PyQt4.QtGui import QFileSystemModel, QTableView
+from PyQt4.QtGui import QFileSystemModel, QTableView, QColor, QFont
 
 from PyQt4.QtCore import Qt, QAbstractTableModel, QModelIndex, QVariant
 
@@ -11,13 +11,10 @@ import mainwindow
 import sys
 import os
 
-class FitsHeaderModel(QAbstractTableModel):
+class FitsHeaderListModel(QAbstractTableModel):
     def __init__(self, fileUrl):
         QAbstractTableModel.__init__(self)
         self.hdulist = pyfits.open(str(fileUrl))
-
-        self.setHeaderData(0, Qt.Horizontal, "Name")
-        self.setHeaderData(1, Qt.Horizontal, "Shape")
 
     def rowCount(self, parent):
         try:
@@ -28,6 +25,17 @@ class FitsHeaderModel(QAbstractTableModel):
     def columnCount(self, parent):
         return 2
 
+    def hduEntryForIndex(self, modelIndex):
+        return self.hdulist[modelIndex.row()]
+
+    def headerData(self, section, orientation, role):
+        if role == Qt.DisplayRole and orientation == Qt.Horizontal:
+            if section == 0:
+                return "Name"
+            if section == 1:
+                return "Shape"
+        return QAbstractTableModel.headerData(self, section, orientation, role)
+
     def data(self, index, role):
         if role == Qt.DisplayRole:
             entry = self.hdulist[index.row()]
@@ -35,10 +43,79 @@ class FitsHeaderModel(QAbstractTableModel):
                 return entry.name
             if index.column() == 1:
                 try:
-                    return "[{0}x{1}]".format(len(entry.columns), len(entry.data))
+                    return "{0}x{1}".format(len(entry.columns), len(entry.data))
                 except AttributeError:
                     return "---"
-        return None
+        if role == Qt.ForegroundRole:
+            if index.column() == 1:
+                return QColor("#8F8F8F")
+        if role == Qt.FontRole:
+            if index.column() == 0:
+                f = QFont()
+                f.setBold(True)
+                return f
+
+class FitsHeaderModel(QAbstractTableModel):
+    def __init__(self, hduentry):
+        QAbstractTableModel.__init__(self)
+        self.header = hduentry.header
+
+    def rowCount(self, parent):
+        return len(self.header)
+
+    def columnCount(self, parent):
+        return 3
+
+    def headerData(self, section, orientation, role):
+        if role == Qt.DisplayRole and orientation == Qt.Horizontal:
+            if section == 0:
+                return "Key"
+            if section == 1:
+                return "Value"
+            if section == 2:
+                return "Comment"
+        return QAbstractTableModel.headerData(self, section, orientation, role)
+
+    def data(self, index, role):
+        if role == Qt.DisplayRole:
+            if index.column() == 0:
+                return self.header.keys()[index.row()]
+            if index.column() == 1:
+                return self.header.values()[index.row()]
+            if index.column() == 2:
+                return self.header.comments[self.header.keys()[index.row()]]
+
+        if role == Qt.ForegroundRole:
+            if index.column() == 1:
+                return QColor("#900059")
+            if index.column() == 2:
+                return QColor("#8F8F8F")
+
+class FitsDataModel(QAbstractTableModel):
+    def __init__(self, hduentry):
+        QAbstractTableModel.__init__(self)
+        self.fitsdata = hduentry.data
+
+    def rowCount(self, parent):
+        if self.fitsdata is not None:
+            return len(self.fitsdata)
+        return 0
+
+    def columnCount(self, parent):
+        if self.fitsdata is not None:
+            return len(self.fitsdata.columns)
+        return 0
+
+    def headerData(self, section, orientation, role):
+        if role == Qt.DisplayRole and orientation == Qt.Horizontal:
+            item = self.fitsdata.columns[section]
+            return "{0} / {1} [{2}]".format(item.name, item.format, item.unit)
+        return QAbstractTableModel.headerData(self, section, orientation, role)
+
+    def data(self, index, role):
+        if role == Qt.DisplayRole:
+            data = self.fitsdata[index.row()][index.column()]
+            return str(data)
 
 class FitsViewer(QMainWindow):
     def __init__(self):
@@ -59,6 +136,26 @@ class FitsViewer(QMainWindow):
         self.ui.files.selectionModel().selectionChanged.connect(self.fileSelected)
 
         self.ui.sections.setSelectionBehavior(QTableView.SelectRows)
+        self.ui.sections.verticalHeader().setDefaultSectionSize(20)
+        self.ui.sections.setShowGrid(False)
+        self.ui.header.setShowGrid(False)
+        self.ui.sections.setStyleSheet("QTableView::item { padding: 25px }")
+        self.ui.header.setStyleSheet("QTableView::item { padding: 8px }")
+        self.ui.contents.setStyleSheet("QTableView::item { padding: 8px }")
+
+        self.ui.splitter.setSizes([200, 500])
+        self.ui.splitter_2.setSizes([200, 500])
+        self.ui.splitter_3.setSizes([200, 500])
+
+    def hduSelected(self, item):
+        hduEntry = self.hduListModel.hduEntryForIndex(item)
+        self.ui.header.setModel(FitsHeaderModel(hduEntry))
+        self.ui.header.resizeColumnsToContents()
+        self.ui.header.verticalHeader().setDefaultSectionSize(20)
+
+        self.ui.contents.setModel(FitsDataModel(hduEntry))
+        self.ui.contents.resizeColumnsToContents()
+        self.ui.contents.verticalHeader().setDefaultSectionSize(20)
 
     def fileSelected(self):
         files = self.ui.files.selectionModel().selectedIndexes()
@@ -68,8 +165,11 @@ class FitsViewer(QMainWindow):
             print "No file selected, huh?"
             return
         self.currentFile = self.ui.url.text() + "/" + filename
-        self.ui.sections.setModel(FitsHeaderModel(self.currentFile))
+        self.hduListModel = FitsHeaderListModel(self.currentFile)
+
+        self.ui.sections.setModel(self.hduListModel)
         self.ui.sections.resizeColumnsToContents()
+        self.ui.sections.selectionModel().currentChanged.connect(self.hduSelected)
 
     def pathChanged(self):
         index = self.fileModel.setRootPath(self.ui.url.text())
