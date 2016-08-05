f617e64e4c9ca00da3c63a578a456b30c8f6a9fb
bwinkel <bwinkel78@gmail.com>
2016-08-05 13:11:47 +0200

    pep8; code style and py3 compat

--- compileform.sh
+++ compileform.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-pyuic4 mainwindow.ui -o mainwindow.py
-pyuic4 plotwindow.ui -o plotwindow.py
+export ANACONDA=/home/bwinkel/local/anaconda2/
+alias apyuic="PYTHONPATH= LDFLAGS= CFLAGS= LD_LIBRARY_PATH= QT5DIR= $ANACONDA/bin/pyuic4"
+
+apyuic mainwindow_form.ui -o mainwindow_form.py
+apyuic plotwindow_form.ui -o plotwindow_form.py
